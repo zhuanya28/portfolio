f675de92873368831b2d0c799b2f42984b51dccc
anya <azhukova1402@gmail.com>
2024-02-07 19:52:56 -0500

    added: the previous version

---
+++ my-react-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './index.css';
+import Menu from "./components/Menu"
+import Contact from "./components/Contact"
+
+
+function App() {
+  return (
+    <div>
+      <Contact />
+      <Menu />
+    </div>
+  );
+}
+
+
+export default App;
+
+
